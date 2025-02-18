2a7d46ea2254fdbdd970097eb4e5fe1be066011b
bilal-ahmed07 <bilalahmed.15952@gmail.com>
2025-02-19 00:07:34 +0500

    Connecting Supabase

---
+++ lib/core/Creds/creds.dart
@@ -0,0 +1,4 @@
+class Creds {
+  static const supabaseUrl = "https://kbbeoljudfvovazlbqun.supabase.co";
+  static const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiYmVvbGp1ZGZ2b3ZhemxicXVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5MDE0MDksImV4cCI6MjA1NTQ3NzQwOX0.iwlMj-OnZfyR7HPnEpFG1_AQMosjA8sA6P2GedjJ0zk";
+}
